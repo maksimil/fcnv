2a2b675c539e07db8343b8b4447ff420a33f5191
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-12-17 14:05:11 +0300

    Replaced --files and --pngs with --mode

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,25 @@ pub mod ft;
 
 use c128::{Complex, PI, TPI};
 
+enum Mode {
+    Pngs,
+    Svg,
+    Svgs,
+    // Gif,
+}
+
+impl From<&str> for Mode {
+    fn from(s: &str) -> Mode {
+        match s {
+            "pngs" => Mode::Pngs,
+            "svg" => Mode::Svg,
+            "svgs" => Mode::Svgs,
+            // "gif" => Mode::Gif,
+            s => panic!("No matching mode found for option {}", s),
+        }
+    }
+}
+
 fn main() {
     let matches = clap_app!(fncv =>
         (version: "1.0")
@@ -21,13 +40,17 @@ fn main() {
         (@arg FRAMES: -f --frames +takes_value "Sets the number of frames in a full circle")
         (@arg DURATION: --dur +takes_value "Sets the duration of the animation")
         (@arg DEPTH: -d --depth +takes_value "Sets the depth of transform")
-        (@arg FILES: --files "Sets output mode to output frames instead of animation")
         (@arg MERGE: --merge "Merges all the paths in file to a single path")
         (@arg OFFSET: --offset #{2,2} "Sets offset")
         (@arg STROKE_WIDTH: --sw +takes_value "Sets stroke width in svg")
-        (@arg PNGS: --pngs "Outputs files as pngs")
+        (@arg MODE: -m --mode +takes_value "Sets the output mode (svg/svgs/pngs)")
     )
     .get_matches();
+
+    let mode = matches
+        .value_of("MODE")
+        .map(Mode::from)
+        .unwrap_or(Mode::Svg);
 
     let sw = matches
         .value_of("STROKE_WIDTH")
@@ -114,191 +137,205 @@ fn main() {
         .parse::<usize>()
         .expect("Was not able to parse a usize from DEPTH argument");
 
-    let pngs = matches.is_present("PNGS");
+    let c = transform(path, depth);
 
-    if matches.is_present("FILES") || pngs {
-        // several files
-        let c = transform(path, depth);
+    match mode {
+        Mode::Svg => {
+            let frames = matches
+                .value_of("FRAMES")
+                .unwrap_or("600")
+                .parse::<usize>()
+                .expect("Was not able to parse a usize from FRAMES argument");
+            let time = matches
+                .value_of("DURATION")
+                .unwrap_or("10")
+                .parse::<usize>()
+                .expect("Was not able to parse a usize from DURATION argument");
 
-        let frames = matches
-            .value_of("FRAMES")
-            .unwrap_or("600")
-            .parse::<usize>()
-            .expect("Was not able to parse a usize from FRAMES argument");
+            let default_output_path = format!("{}_.svg", fname);
 
-        let default_output_path = format!("{}_frames", fname);
+            let out_fp = matches.value_of("OUTPUT").unwrap_or(&default_output_path);
 
-        let out_fp = matches.value_of("OUTPUT").unwrap_or(&default_output_path);
-
-        let mut pstring = String::new();
-
-        for frame in 0..=frames {
-            let t = (frame as f64) / (frames as f64);
-
-            let last = {
-                let mut last = c[0];
-
-                for i in 1..c.len() {
-                    last = last + c[i] * Complex::ei(TPI * unindex(i) * t + PI);
-                }
-
-                last
-            };
-
-            if frame == 0 {
-                pstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
-            } else {
-                pstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
-            }
-        }
-
-        let mut lpstring = String::new();
-
-        for frame in 0..=frames {
             let mut dstring = String::new();
+            let mut tstring = String::new();
 
-            let t = (frame as f64) / (frames as f64);
+            // path
+            let mut lpstring = String::new();
+            let mut pstring = String::new();
 
-            let last = {
-                let mut last = c[0];
+            for frame in 0..=frames {
+                let t = (frame as f64) / (frames as f64);
 
-                dstring.push_str(&format!("M {} {} ", last.x + offset[0], last.y + offset[1]));
+                tstring.push_str(&format!("{};", t.to_string()));
 
-                for i in 1..c.len() {
-                    last = last + c[i] * Complex::ei(TPI * unindex(i) * t + PI);
+                let last = {
+                    let mut last = c[0];
 
-                    dstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
-                }
+                    dstring.push_str(&format!("M {} {} ", last.x + offset[0], last.y + offset[1]));
 
-                last
-            };
+                    for i in 1..c.len() {
+                        last = last + c[i] * Complex::ei(TPI * unindex(i) * t + PI);
 
-            if frame == 0 {
-                lpstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
-            } else {
-                lpstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
-            }
+                        dstring.push_str(&format!(
+                            "L {} {}",
+                            last.x + offset[0],
+                            last.y + offset[1]
+                        ));
+                    }
 
-            {
-                let svg = format!(
-                    "
-            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{width}\" height=\"{height}\"><g>
-            <path d=\"{lpstring}\" stroke-width=\"{sw}\" stroke=\"#0022e4\" fill=\"none\"/>
-            <path d=\"{dstring}\" stroke-width=\"{sw}\" stroke=\"#000\" fill=\"none\" />
-            </g></svg>",
-                    dstring = dstring,
-                    lpstring = lpstring,
-                    width = width,
-                    height = height,
-                    sw = sw
-                );
+                    last
+                };
 
-                if pngs {
-                    let tree = Tree::from_str(&svg, &Options::default())
-                        .expect("Failed to parse generated svg");
-                    render(&tree, usvg::FitTo::Original, None)
-                        .expect("Failed to render generated svg")
-                        .save_png(format!("{}/frame-{}.png", out_fp, frame))
-                        .expect("Failed to save png");
+                if frame == 0 {
+                    lpstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
                 } else {
-                    write(format!("{}/frame-{}.svg", out_fp, frame), svg)
-                        .expect("Unable to save file");
-                }
-            }
-
-            {
-                let svg = format!(
-                    "
-            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{width}\" height=\"{height}\"><g>
-            <path d=\"{lpstring}\" stroke-width=\"{sw}\" stroke=\"#0022e4\" fill=\"none\"/>
-            <path d=\"{dstring}\" stroke-width=\"{sw}\" stroke=\"#000\" fill=\"none\" />
-            </g></svg>",
-                    dstring = dstring,
-                    lpstring = pstring,
-                    width = width,
-                    height = height,
-                    sw = sw
-                );
-
-                if pngs {
-                    let tree = Tree::from_str(&svg, &Options::default())
-                        .expect("Failed to parse generated svg");
-                    render(&tree, usvg::FitTo::Original, None)
-                        .expect("Failed to render generated svg")
-                        .save_png(format!("{}/frame-{}.png", out_fp, frame + frames))
-                        .expect("Failed to save png");
-                } else {
-                    write(format!("{}/frame-{}.svg", out_fp, frame + frames), svg)
-                        .expect("Unable to save file");
-                }
-            }
-        }
-    } else {
-        let c = transform(path, depth);
-
-        let frames = matches
-            .value_of("FRAMES")
-            .unwrap_or("600")
-            .parse::<usize>()
-            .expect("Was not able to parse a usize from FRAMES argument");
-        let time = matches
-            .value_of("DURATION")
-            .unwrap_or("10")
-            .parse::<usize>()
-            .expect("Was not able to parse a usize from DURATION argument");
-
-        let default_output_path = format!("{}_.svg", fname);
-
-        let out_fp = matches.value_of("OUTPUT").unwrap_or(&default_output_path);
-
-        let mut dstring = String::new();
-        let mut tstring = String::new();
-
-        // path
-        let mut lpstring = String::new();
-        let mut pstring = String::new();
-
-        for frame in 0..=frames {
-            let t = (frame as f64) / (frames as f64);
-
-            tstring.push_str(&format!("{};", t.to_string()));
-
-            let last = {
-                let mut last = c[0];
-
-                dstring.push_str(&format!("M {} {} ", last.x + offset[0], last.y + offset[1]));
-
-                for i in 1..c.len() {
-                    last = last + c[i] * Complex::ei(TPI * unindex(i) * t + PI);
-
-                    dstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
+                    lpstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
                 }
 
-                last
-            };
+                pstring.push_str(&lpstring);
 
-            if frame == 0 {
-                lpstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
-            } else {
-                lpstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
+                dstring.push_str(";");
+                pstring.push_str(";");
             }
 
-            pstring.push_str(&lpstring);
+            tstring.pop();
+            dstring.pop();
+            pstring.pop();
 
-            dstring.push_str(";");
-            pstring.push_str(";");
-        }
-
-        tstring.pop();
-        dstring.pop();
-        pstring.pop();
-
-        let svg = format!("
+            let svg = format!("
 <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{width}\" height=\"{height}\"><g>
 <path d=\"{lpstring}\" stroke-width=\"{sw}\" stroke=\"#0022e4\" fill=\"none\"><animate attributeName=\"d\" values=\"{pstring}\" keyTimes=\"{tstring}\" dur=\"{time}s\" begin=\"0s\" repeatCount=\"1\"/></path>
 <path d=\"\" stroke-width=\"{sw}\" stroke=\"#000\" fill=\"none\"><animate attributeName=\"d\" values=\"{dstring}\" keyTimes=\"{tstring}\" dur=\"{time}s\" begin=\"0s\" repeatCount=\"indefinite\"/></path>
 </g></svg>", dstring=dstring, tstring=tstring, time=time,lpstring=lpstring, pstring=pstring, width = width, height = height, sw = sw);
 
-        write(out_fp, svg).expect("Unable to save file");
+            write(out_fp, svg).expect("Unable to save file");
+        }
+        mode => {
+            let frames = matches
+                .value_of("FRAMES")
+                .unwrap_or("600")
+                .parse::<usize>()
+                .expect("Was not able to parse a usize from FRAMES argument");
+
+            let default_output_path = format!("{}_frames", fname);
+
+            let out_fp = matches.value_of("OUTPUT").unwrap_or(&default_output_path);
+
+            let mut pstring = String::new();
+
+            for frame in 0..=frames {
+                let t = (frame as f64) / (frames as f64);
+
+                let last = {
+                    let mut last = c[0];
+
+                    for i in 1..c.len() {
+                        last = last + c[i] * Complex::ei(TPI * unindex(i) * t + PI);
+                    }
+
+                    last
+                };
+
+                if frame == 0 {
+                    pstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
+                } else {
+                    pstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
+                }
+            }
+
+            let mut lpstring = String::new();
+
+            for frame in 0..=frames {
+                let mut dstring = String::new();
+
+                let t = (frame as f64) / (frames as f64);
+
+                let last = {
+                    let mut last = c[0];
+
+                    dstring.push_str(&format!("M {} {} ", last.x + offset[0], last.y + offset[1]));
+
+                    for i in 1..c.len() {
+                        last = last + c[i] * Complex::ei(TPI * unindex(i) * t + PI);
+
+                        dstring.push_str(&format!(
+                            "L {} {}",
+                            last.x + offset[0],
+                            last.y + offset[1]
+                        ));
+                    }
+
+                    last
+                };
+
+                if frame == 0 {
+                    lpstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
+                } else {
+                    lpstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
+                }
+
+                {
+                    let svg = format!(
+                        "
+            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{width}\" height=\"{height}\"><g>
+            <path d=\"{lpstring}\" stroke-width=\"{sw}\" stroke=\"#0022e4\" fill=\"none\"/>
+            <path d=\"{dstring}\" stroke-width=\"{sw}\" stroke=\"#000\" fill=\"none\" />
+            </g></svg>",
+                        dstring = dstring,
+                        lpstring = lpstring,
+                        width = width,
+                        height = height,
+                        sw = sw
+                    );
+
+                    match mode {
+                        Mode::Svgs => {
+                            write(format!("{}/frame-{}.svg", out_fp, frame), svg)
+                                .expect("Unable to save file");
+                        }
+                        Mode::Pngs => {
+                            let tree = Tree::from_str(&svg, &Options::default())
+                                .expect("Failed to parse generated svg");
+                            render(&tree, usvg::FitTo::Original, None)
+                                .expect("Failed to render generated svg")
+                                .save_png(format!("{}/frame-{}.png", out_fp, frame))
+                                .expect("Failed to save png");
+                        }
+                        _ => {}
+                    }
+                }
+
+                {
+                    let svg = format!(
+                        "
+            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{width}\" height=\"{height}\"><g>
+            <path d=\"{lpstring}\" stroke-width=\"{sw}\" stroke=\"#0022e4\" fill=\"none\"/>
+            <path d=\"{dstring}\" stroke-width=\"{sw}\" stroke=\"#000\" fill=\"none\" />
+            </g></svg>",
+                        dstring = dstring,
+                        lpstring = pstring,
+                        width = width,
+                        height = height,
+                        sw = sw
+                    );
+
+                    match mode {
+                        Mode::Svgs => {
+                            write(format!("{}/frame-{}.svg", out_fp, frame + frames), svg)
+                                .expect("Unable to save file");
+                        }
+                        Mode::Pngs => {
+                            let tree = Tree::from_str(&svg, &Options::default())
+                                .expect("Failed to parse generated svg");
+                            render(&tree, usvg::FitTo::Original, None)
+                                .expect("Failed to render generated svg")
+                                .save_png(format!("{}/frame-{}.png", out_fp, frame + frames))
+                                .expect("Failed to save png");
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
     }
 }
