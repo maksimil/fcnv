3cb0ef031a87db626c7aa98382619b42cfbb8758
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-12-17 15:38:40 +0300

    Cleaned up code a bit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use clap::clap_app;
+use clap::{clap_app, ArgMatches};
 use ft::{transform, unindex};
 use quick_xml::{events::Event, Reader};
 use resvg::render;
-use std::fs::{read_to_string, write};
+use std::{
+    any::type_name,
+    fs::{read_to_string, write},
+    str::FromStr,
+};
 use svg2polylines::parse;
 use usvg::{Options, Tree};
 
@@ -10,6 +14,29 @@ pub mod c128;
 pub mod ft;
 
 use c128::{Complex, PI, TPI};
+
+// Error messages
+const SAVE_FAIL: &str = "Failed to save file";
+
+fn parse_fail(t: &str, arg: &str) -> String {
+    format!("Failed to parse {} from {}", t, arg)
+}
+
+fn get_arg<T: FromStr>(matches: &ArgMatches, arg: &str, default: T) -> T
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    matches
+        .value_of(arg)
+        .map(|v| {
+            v.parse::<T>().expect(&format!(
+                "Failed to parse {} from {}",
+                type_name::<T>(),
+                arg
+            ))
+        })
+        .unwrap_or(default)
+}
 
 enum Mode {
     Pngs,
@@ -47,127 +74,133 @@ fn main() {
     )
     .get_matches();
 
+    // Get mode of output
     let mode = matches
         .value_of("MODE")
         .map(Mode::from)
         .unwrap_or(Mode::Svg);
 
-    let sw = matches
-        .value_of("STROKE_WIDTH")
-        .map(|v| v.parse::<f64>().expect("Failed to parse f64 from --sw arg"))
-        .unwrap_or(1.5);
+    // Parsing file
+    let fname = matches.value_of("FILE").expect("Could not get file name");
+    let s = read_to_string(fname).expect("Could not read .svg file");
+    let path = {
+        let mut plines = parse(&s).expect("Could not parse .svg file");
+        if plines.len() == 0 {
+            panic!("No lines found in file");
+        }
 
+        if matches.is_present("MERGE") {
+            plines.into_iter().fold(Vec::new(), |mut acc, val| {
+                acc.extend(val.into_iter().map(Complex::from));
+                acc
+            })
+        } else {
+            plines
+                .swap_remove(0)
+                .into_iter()
+                .map(Complex::from)
+                .collect()
+        }
+    };
+
+    // Getting svg style things
+    // Get stroke width
+    let sw = get_arg(&matches, "STROKE_WIDTH", 1.5);
+
+    // Get offset
     let offset = matches
         .values_of("OFFSET")
         .map(|vs| {
-            vs.map(|v| {
-                v.parse::<f64>()
-                    .expect("Failed to parse f64 from offset parameter")
-            })
-            .collect::<Vec<_>>()
+            vs.map(|v| v.parse::<f64>().expect(&parse_fail("f64", "OFFSET")))
+                .collect::<Vec<_>>()
         })
         .unwrap_or(vec![0.0, 0.0]);
 
-    let fname = matches.value_of("FILE").expect("Could not get file name");
-
-    let s = read_to_string(fname).expect("Could not read .svg file");
-
-    let mut plines = parse(&s).expect("Could not parse .svg file");
-
+    // Get width and height
     let (width, height) = {
         let mut width = String::new();
         let mut height = String::new();
         let mut reader = Reader::from_str(&s);
-        // let mut txt = Vec::new();
         let mut buff = Vec::new();
 
         loop {
             match reader.read_event(&mut buff) {
-                Ok(Event::Start(ref e)) => match e.name() {
-                    b"svg" => {
-                        for attr in e.attributes() {
-                            if let Ok(attr) = attr {
-                                let key = String::from_utf8_lossy(attr.key);
-                                if key == "width" {
-                                    width = String::from_utf8(attr.value.to_vec())
-                                        .expect("Failed to parse svg width");
-                                } else if key == "height" {
-                                    height = String::from_utf8(attr.value.to_vec())
-                                        .expect("Failed to parse svg height");
+                Ok(Event::Start(ref e)) => {
+                    match e.name() {
+                        b"svg" => {
+                            for attr in e.attributes() {
+                                if let Ok(attr) = attr {
+                                    let key = String::from_utf8_lossy(attr.key);
+                                    if key == "width" {
+                                        width = String::from_utf8(attr.value.to_vec())
+                                            .expect("Failed to parse svg width");
+                                    } else if key == "height" {
+                                        height = String::from_utf8(attr.value.to_vec())
+                                            .expect("Failed to parse svg height");
+                                    }
                                 }
-
+                            }
+                            // Panic if cannot offset due to units
+                            if offset[0] != 0.0 && offset[1] != 0.0 {
                                 if let Ok(pw) = width.parse::<f64>() {
                                     width = (pw + offset[0]).to_string();
+                                } else {
+                                    panic!("Width with value {} is not supported with offset due to units", width);
                                 }
 
                                 if let Ok(ph) = height.parse::<f64>() {
                                     height = (ph + offset[1]).to_string();
+                                } else {
+                                    panic!("Height with value {} is not supported with offset due to units", height);
                                 }
                             }
+                            break (width, height);
                         }
-                        break (width, height);
+                        _ => {}
                     }
-                    _ => {}
-                },
+                }
                 _ => {}
             }
         }
     };
 
-    if plines.len() == 0 {
-        panic!("No lines found in file");
-    }
+    // Get frame count
+    let frames = get_arg::<usize>(&matches, "FRAMES", 600);
 
-    let path: Vec<Complex> = if matches.is_present("MERGE") {
-        plines.into_iter().fold(Vec::new(), |mut acc, val| {
-            acc.extend(val.into_iter().map(Complex::from));
-            acc
-        })
-    } else {
-        plines
-            .swap_remove(0)
-            .into_iter()
-            .map(Complex::from)
-            .collect()
-    };
+    // Transform
+    // Get depth
+    let depth = get_arg::<usize>(&matches, "DEPTH", 100);
 
-    let depth = matches
-        .value_of("DEPTH")
-        .unwrap_or("100")
-        .parse::<usize>()
-        .expect("Was not able to parse a usize from DEPTH argument");
-
+    // Get coefficients
     let c = transform(path, depth);
 
     match mode {
+        // Svg animation
         Mode::Svg => {
-            let frames = matches
-                .value_of("FRAMES")
-                .unwrap_or("600")
-                .parse::<usize>()
-                .expect("Was not able to parse a usize from FRAMES argument");
-            let time = matches
-                .value_of("DURATION")
-                .unwrap_or("10")
-                .parse::<usize>()
-                .expect("Was not able to parse a usize from DURATION argument");
+            // Animation duration
+            let duration = get_arg(&matches, "DURATION", 10.0);
 
+            // Getting save path
             let default_output_path = format!("{}_.svg", fname);
-
             let out_fp = matches.value_of("OUTPUT").unwrap_or(&default_output_path);
 
+            // Arm and time
             let mut dstring = String::new();
             let mut tstring = String::new();
 
-            // path
+            // Path
             let mut lpstring = String::new();
             let mut pstring = String::new();
 
+            // Iterating through frames
             for frame in 0..=frames {
+                // Time
                 let t = (frame as f64) / (frames as f64);
 
+                // Adding timestamp
                 tstring.push_str(&format!("{};", t.to_string()));
 
+                // Arm pos
                 let last = {
                     let mut last = c[0];
 
@@ -186,46 +219,48 @@ fn main() {
                     last
                 };
 
+                // Pushing pointer pos to last path string
                 if frame == 0 {
                     lpstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
                 } else {
                     lpstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
                 }
 
+                // Pushing last path string to path string for animation
                 pstring.push_str(&lpstring);
 
+                // Pushing ; for animation
                 dstring.push_str(";");
                 pstring.push_str(";");
             }
 
+            // Removing last ;
             tstring.pop();
             dstring.pop();
             pstring.pop();
 
+            // Svg generation and writing
             let svg = format!("
 <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{width}\" height=\"{height}\"><g>
 <path d=\"{lpstring}\" stroke-width=\"{sw}\" stroke=\"#0022e4\" fill=\"none\"><animate attributeName=\"d\" values=\"{pstring}\" keyTimes=\"{tstring}\" dur=\"{time}s\" begin=\"0s\" repeatCount=\"1\"/></path>
 <path d=\"\" stroke-width=\"{sw}\" stroke=\"#000\" fill=\"none\"><animate attributeName=\"d\" values=\"{dstring}\" keyTimes=\"{tstring}\" dur=\"{time}s\" begin=\"0s\" repeatCount=\"indefinite\"/></path>
-</g></svg>", dstring=dstring, tstring=tstring, time=time,lpstring=lpstring, pstring=pstring, width = width, height = height, sw = sw);
+</g></svg>", dstring=dstring, tstring=tstring, time=duration,lpstring=lpstring, pstring=pstring, width = width, height = height, sw = sw);
 
-            write(out_fp, svg).expect("Unable to save file");
+            write(out_fp, svg).expect(SAVE_FAIL);
         }
         mode => {
-            let frames = matches
-                .value_of("FRAMES")
-                .unwrap_or("600")
-                .parse::<usize>()
-                .expect("Was not able to parse a usize from FRAMES argument");
-
+            // Getting save path
             let default_output_path = format!("{}_frames", fname);
-
             let out_fp = matches.value_of("OUTPUT").unwrap_or(&default_output_path);
 
+            // Getting final path string
             let mut pstring = String::new();
 
             for frame in 0..=frames {
+                // Time
                 let t = (frame as f64) / (frames as f64);
 
+                // Pointer pos
                 let last = {
                     let mut last = c[0];
 
@@ -236,6 +271,7 @@ fn main() {
                     last
                 };
 
+                // Pushing pointer pos
                 if frame == 0 {
                     pstring.push_str(&format!("M {} {}", last.x + offset[0], last.y + offset[1]));
                 } else {
@@ -243,13 +279,17 @@ fn main() {
                 }
             }
 
+            // Getting path strings for every frame
             let mut lpstring = String::new();
 
             for frame in 0..=frames {
+                // Arm string
                 let mut dstring = String::new();
 
+                // Time
                 let t = (frame as f64) / (frames as f64);
 
+                // Arm pos
                 let last = {
                     let mut last = c[0];
 
@@ -274,6 +314,7 @@ fn main() {
                     lpstring.push_str(&format!("L {} {}", last.x + offset[0], last.y + offset[1]));
                 }
 
+                // Generating frames before the full circle (path is lpstring)
                 {
                     let svg = format!(
                         "
@@ -305,6 +346,7 @@ fn main() {
                     }
                 }
 
+                // Generating frames after the full circle (path is pstring)
                 {
                     let svg = format!(
                         "
